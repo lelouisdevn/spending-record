cd46384233f972007c3ad3092095d9bc385e08e8
lelouisdevn <atlanteansvietnam@outlook.com>
2023-07-29 16:40:06 +0700

    Use method call to "login" on "User" object; change to "switch" on "signUp" method;

--- user/user.cpp
+++ user/user.cpp
@@ -12,7 +12,7 @@ sql::Connection *con;
 sql::PreparedStatement *pstmt;
 sql::ResultSet *res;
 class User {
-    string userId;
+    int userId;
     string username;
     string password;
     string email;
@@ -27,12 +27,20 @@ class User {
     };
 
 public:
+    User() {
+        userId = -1;
+        username = "";
+        password = "";
+        email = "";
+    }
     User(string username, string email, string password) {
         this->username = username;
         this->email = email;
         this->password = password;
     }
-
+    void printInfo() {
+        cout << username << " " << email << endl;
+    }
     // Method to check if username or email exists in database;
     int isAccountExisted() {
         int response_code = 0;
@@ -60,26 +68,70 @@ public:
         }
         return response_code;
     }
+
+    // int ()
+
+    // Create new user with submitted info;
     void signUp() {
         int status = isAccountExisted();
-        if (status == 0) {
-            try {
-                pstmt = con->prepareStatement("INSERT INTO user (username, password, email) VALUES (?, ?, ?)");
-                pstmt->setString(1, username);
-                pstmt->setString(2, password);
-                pstmt->setString(3, email);
+        switch(status) {
+            case 0:
+                try {
+                    pstmt = con->prepareStatement("INSERT INTO user (username, password, email) VALUES (?, ?, ?)");
+                    pstmt->setString(1, username);
+                    pstmt->setString(2, password);
+                    pstmt->setString(3, email);
 
-                status = pstmt->executeUpdate();
-                if (!status) {
-                    throw(status);
-                }else {
-                   cout << "Your account is created successfully!" << endl; 
+                    status = pstmt->executeUpdate();
+                    if (!status) {
+                        throw(status);
+                    }else {
+                        cout << "Your account is created successfully!" << endl;
+                        login();
+                    }
+                } catch(int err) {
+                    cout << "There occured an error: " << err << endl;
                 }
-            }catch(int err) {
-                cout << "There occured an error: " << err << endl;
-            }
-        } else {
-            cout << this->errors[status] << endl;
+                break;
+            case 1:
+            case 2:
+                cout << this->errors[status] << endl;
+                break;
+            default:
+                cout << "Undefined error occured!" << endl;
+                break;
         }
+    }
+
+    void login() {
+        try {
+            driver = get_driver_instance();
+            con = driver->connect("tcp://127.0.0.1:3306", "root", "");
+            con->setSchema("spending-record");
+
+            pstmt = con->prepareStatement("SELECT * FROM user WHERE (email=(?) OR username=(?)) AND password=(?)");
+            pstmt->setString(1, email);
+            pstmt->setString(2, username);
+            pstmt->setString(3, password);
+
+            res = pstmt->executeQuery();
+            while (res->next()) {
+                int uid = res->getInt("userId");
+                string usr = res->getString("username");
+                string eml = res->getString("email");
+                update_attrs(uid, usr, eml, "");
+                
+                break;
+            }
+        } catch(sql::SQLException &e) {
+            cout << e.what() << endl;
+        }
+    }
+
+    void update_attrs(int uid, string usr, string eml, string passwd) {
+        this->userId = uid;
+        this->username = usr;
+        this->email = eml;
+        this->password = passwd;
     }
 };
